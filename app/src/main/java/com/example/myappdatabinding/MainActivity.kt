b414ab45bc8faa0adbb9e3154d07d18a000e2ca9
NaokiArakawa <naoki.arakawa1026@gmail.com>
2021-12-30 16:28:55 +0900

    FragmentとDataBinding

--- app/src/main/java/com/example/myappdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/myappdatabinding/MainActivity.kt
@@ -1,19 +1,9 @@
 package com.example.myappdatabinding
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.example.myappdatabinding.databinding.ActivityMainBinding
-import com.example.myappdatabinding.databinding.ListExampleBinding
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,14 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val adapter = MainListAdapter(this).apply {
-            add(ListItem("ゴミ出し", Date()))
-            add(ListItem("技術書を書く", Date()))
-            add(ListItem("脱稿する", Date()))
-        }
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.listView.adapter = adapter
         setContentView(binding.root)
     }
 
